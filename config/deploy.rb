05ffffc024596cde3c72f967cd1fb7949012f8b4
syukihiro <kaui.yukihiro@gmail.com>
2019-06-15 13:23:01 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url,  'git@github.com:syukihiro/chat-space.git'
+set :repo_url, 'git@github.com:syukihiro/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -11,7 +11,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['/.ssh/syukihiro.pem']
-                  
+
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
