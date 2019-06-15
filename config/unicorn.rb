2fe278aeafd6a32220f3263954159cb22f9b98a5
syukihiro <kaui.yukihiro@gmail.com>
2019-06-15 18:23:31 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,25 +6,14 @@ worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
 working_directory "#{app_path}/current"
-
-
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
-
-
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
-
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
-
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
-
-#以下は応用的な設定なので説明は割愛
-
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
 
