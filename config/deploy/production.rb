255e3eb42599a85af770d81d58ae6e42915686ce
syukihiro <kaui.yukihiro@gmail.com>
2019-06-17 09:21:05 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@ server '3.113.148.221', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+end
 # role-based syntax
 # ==================
 
